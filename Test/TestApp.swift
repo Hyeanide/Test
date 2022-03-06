199839bc705563d730715669267f68a6f13811e6
Hyeanide <mat.bednarz97@hotmail.com>
2022-03-06 19:42:34 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Mateusz Bednarz on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
